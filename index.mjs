6123647654273062187c6349a8060c60d6916c86
Bernhard Lukassen <bl@bernhard-lukassen.com>
2019-12-29 12:59:33 +0100

    work on system structure, setup and boot

---
+++ index.mjs
@@ -0,0 +1,8 @@
+/**
+ *
+ *
+ * @author: Bernhard Lukassen
+ */
+
+export { default as NedbObjectStore }   from './lib/nedbobjectstore.mjs';
+export { default as NedbEventStore }    from './lib/nedbeventstore.mjs';
